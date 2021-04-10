d3f0c0e73e9c9af0ec150c06fde1e4605931b946
Augustine Akoto Larbi-Ampofo <alarbiampofo@gmail.com>
2021-04-10 14:51:23 +0000

    check if next one is safe to jump for min jumps

--- jumpingOnClouds.js
+++ jumpingOnClouds.js
@@ -6,8 +6,16 @@ let safe = 0, danger = 1, count = 0
 let jump = 0;
 while(jump<arr.length-1){
   if(arr[jump % 2] === safe && arr[(jump+1)] === safe){
+    //check if next one is safe as well
+    if(arr[jump+2] === safe){
+    count++
+    jump = jump + 2
+    }
+    else{
     count++
     jump = jump + 1
+    }
+   
   }
   else if(arr[jump] === safe && arr[jump+1] === danger){
     count ++
