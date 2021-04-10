eb55faaee847b4fe81fad2c9169af2f8b485082e
Augustine Akoto Larbi-Ampofo <alarbiampofo@gmail.com>
2021-04-10 15:34:00 +0000

    included desc of problem

--- rotateLeft.js
+++ rotateLeft.js
@@ -1,3 +1,8 @@
+/*
+A left rotation operation on an array of size  shifts each of the array's elements
+unit to the left. Given an integer, , rotate the array that many steps left and return the result.
+*/
+
 function rotateLeft(d, arr) {
     // Write your code here
     let temp = []
