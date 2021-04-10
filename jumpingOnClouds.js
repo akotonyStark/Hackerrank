17cab27ff3c5463eb3d3051e6c473b825f64bb44
Augustine Akoto Larbi-Ampofo <alarbiampofo@gmail.com>
2021-04-10 14:49:45 +0000

    Create jumpingOnClouds.js

---
+++ jumpingOnClouds.js
@@ -0,0 +1,23 @@
+
+// Complete the jumpingOnClouds function below.
+function jumpingOnClouds(arr) {
+let safe = 0, danger = 1, count = 0
+
+let jump = 0;
+while(jump<arr.length-1){
+  if(arr[jump % 2] === safe && arr[(jump+1)] === safe){
+    count++
+    jump = jump + 1
+  }
+  else if(arr[jump] === safe && arr[jump+1] === danger){
+    count ++
+    jump = jump + 2
+  }
+  
+}
+return count
+}
+
+let arr = [0,0,0,1,0,0]
+console.log(jumpingOnClouds(arr))
+
