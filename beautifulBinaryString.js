e4c5a55ede60440c9bbc3a69cb7fb11732083514
Augustine Akoto Larbi-Ampofo <alarbiampofo@gmail.com>
2021-04-12 17:34:37 +0000

    Update beautifulBinaryString.js

--- beautifulBinaryString.js
+++ beautifulBinaryString.js
@@ -1,3 +1,10 @@
+/* Alice has a binary string. She thinks a binary string is beautiful if and only if it doesn't contain the substring 010.
+
+In one step, Alice can change a 0 to a 1 or vice versa. Count and print the minimum number of steps needed to make Alice see the string as beautiful.
+
+*/
+
+
 // Complete the beautifulBinaryString function below.
 function beautifulBinaryString(b) {
 
