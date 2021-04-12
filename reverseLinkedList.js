9bb3433f81ddd611c2835bb80ea2efdab3e0909c
Augustine Akoto Larbi-Ampofo <alarbiampofo@gmail.com>
2021-04-12 12:27:58 +0000

    Update reverseLinkedList.js

--- reverseLinkedList.js
+++ reverseLinkedList.js
@@ -1,17 +1,17 @@
 function reverse(head) {
 
-if (head == null || head.next == null) {
-        return head;
-    }
-    let prev = null;
-    let curr = head;
-    let next = null;
-    while (curr != null) {
-        next = curr.next;
-        curr.next = prev; // changes arrow direction
-        prev = curr;
-        curr = next;
-    }
-    return prev;
+        if (head == null || head.next == null) {
+         return head;
+         }
+            let prev = null;
+            let curr = head;
+            let next = null;
+            while (curr != null) {
+                next = curr.next;
+                curr.next = prev;
+                prev = curr;
+                curr = next;
+            }
+            return prev;
 
 }
