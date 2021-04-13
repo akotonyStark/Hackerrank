86fb458271bbdfb87316aecfbb3bda3c9289195a
Augustine Akoto Larbi-Ampofo <alarbiampofo@gmail.com>
2021-04-13 18:54:12 +0000

    fixed the infinite loop bug

--- jumpingOnClouds.js
+++ jumpingOnClouds.js
@@ -1,10 +1,11 @@
-
 // Complete the jumpingOnClouds function below.
+
+
 function jumpingOnClouds(arr) {
 let safe = 0, danger = 1, count = 0
 
 let jump = 0;
-while(jump<arr.length-1){
+for(let i = 0; i<arr.length; i++){
   if(arr[jump] === safe && arr[(jump+1)] === safe){
     //check if next one is safe as well
     if(arr[jump+2] === safe){
@@ -21,11 +22,10 @@ while(jump<arr.length-1){
     count ++
     jump = jump + 2
   }
-  
 }
+  
 return count
 }
 
-let arr = [0,0,0,1,0,0]
-console.log(jumpingOnClouds(arr))
-
+let data = '0 0 1 0 0 0 0 1 0 0 0 0 1 0 0 0 0 0 1 0 1 0 0 0 1 0 0 1 0 0 0 1 0 1 0 0 0 0 0 0 0 0 1 0 0 1 0 1 0 0'.split(' ').map(x => Number(x));
+console.log(jumpingOnClouds(data))
