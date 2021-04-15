121c5fce7cbaa36cc2b6c6072c04ee8ee9673668
Augustine Akoto Larbi-Ampofo <alarbiampofo@gmail.com>
2021-04-15 14:25:55 +0000

    Create ntimesTable.js

---
+++ ntimesTable.js
@@ -0,0 +1,9 @@
+function main() {
+    const n = parseInt(readLine(), 10);
+    
+    
+    for(let i = 1; i<=10; i++){
+        let result = n * i
+        console.log(n, 'x', i,'=',result)
+    }
+}
